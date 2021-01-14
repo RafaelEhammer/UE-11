6297090782e326ad41779169266202a9fcc3cc2e
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2021-01-14 16:32:16 +0100

    implemented class Vehicle

--- src/Model/Vehicle.java
+++ src/Model/Vehicle.java
@@ -2,5 +2,65 @@ package Model;
 
 public class Vehicle
 {
+    private String color;
+    private String owner;
+    private String manufacturer;
+    private String model;
+    private String licensePlate;
 
+    public Vehicle (String color, String owner, String manufacturer, String model, String licensePlate)
+    {
+        this.color = color;
+        this.owner = owner;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.licensePlate = licensePlate;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public String getOwner()
+    {
+        return owner;
+    }
+    public void setOwner(String owner)
+    {
+        this.owner = owner;
+    }
+    public String getManufacturer()
+    {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer)
+    {
+        this.manufacturer = manufacturer;
+    }
+    public String getModel()
+    {
+        return model;
+    }
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+    public String getLicensePlate()
+    {
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate)
+    {
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "color: " + color + "\n" + "owner: " + owner + "\n" + "manufacturer: " + manufacturer + "\n" + "model: " + model + " " + "licensePlate" + licensePlate +"";
+    }
 }
