d1d936fa75b80ef739960bbeca765151778cc582
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2021-01-14 17:03:09 +0100

    started to implement Controller and CarDatabase

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,6 +1,55 @@
 package Controller;
 
-public class Controller
-{
+import Model.CarDatabase;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Controller implements Initializable
+{
+    private Stage stage;
+
+
+    @FXML
+    private TextField TextField;
+    @FXML
+    private TextArea TextArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+    }
+    public void setStage(Stage stage)
+    {
+        this.stage = stage;
+    }
+    public static void show(Stage stage)
+    {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Controller.class.getResource("MainFXML.fxml"));
+            Parent root = fxmlLoader.load();
+
+            //get controller which is connected to this fxml file
+            Controller ctrl = fxmlLoader.getController();
+            ctrl.setStage(stage);
+
+            stage.setTitle("Welcome");
+            stage.setScene(new Scene(root, 400, 400));
+            stage.show();
+        }
+        catch (IOException ex)
+        {
+
+        }
+    }
 }
--- src/Model/CarDatabase.java
+++ src/Model/CarDatabase.java
@@ -1,6 +1,23 @@
 package Model;
 
+import java.util.HashMap;
+
 public class CarDatabase
 {
+    private HashMap<String, Vehicle> db = new HashMap<>();
 
+    public void newVehicle ()
+    {
+        String manufacturer = MagicGenerator.getRandomManufacturer();
+        Vehicle vehicle = new Vehicle(MagicGenerator.getRandomColor(), MagicGenerator.getRandomName(), MagicGenerator.getRandomManufacturer(), MagicGenerator.getRandomModel(manufacturer), MagicGenerator.getRandomLicencePlate());
+        db.put(vehicle.getLicensePlate(),vehicle);
+    }
+
+    public void db ()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+                newVehicle();
+        }
+    }
 }
